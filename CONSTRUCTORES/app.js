e6bc906ac0da86d65109fc0f2daa1db9f0a3315b
Jesus Lopez <jesuslv2412@hotmail.com>
2023-08-08 17:13:07 -0600

    constructores inicio

---
+++ CONSTRUCTORES/app.js
@@ -0,0 +1,22 @@
+// object literal
+
+/*const cliente = {
+    nombre: "Jesus Lopez",
+    saldo: 1711,
+    tipoCliente: function(){
+        let tipo;
+        if(this.saldo > 1000){
+            tipo = 'Gold';
+        } else if(this.saldo > 500) {
+            tipo = 'Platinimu';
+        } else {
+            tipo = 'Normal';
+        }
+
+        return tipo;
+    }
+}
+
+console.log(cliente.tipoCliente());*/
+
+// Metodo alternatico 
